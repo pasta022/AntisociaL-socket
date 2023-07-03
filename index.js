c695235a8cae886e36336a2d4781be2dd25b1aaf
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-07-03 20:20:24 +0100

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-const io = require("socket.io")(8800, {
+const io = require("socket.io")(8080, {
     cors: {
-        origin: "http://localhost/3000"
+        origin: "http://localhost:3000"
     }
 });
 
+let users = [];
+
+//add user
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId === userId) && users.push({ userId, socketId });
+}
+
+//remove user
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+//get receiver
+const getReceiver = (receiverId) => {
+    return users.find(user => user.userId === receiverId);
+}
+
 io.on("connection", (socket) => {
-    console.log("a user has connected");
-});
+    //on connection
+    console.log("user connected");
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send and get message
+    socket.on("sendMessage", ({senderId, receiverId, text}) => {
+        const user = getReceiver(receiverId);
+        io.to(user.socketId).emit("getMessage", { senderId, text });
+    })
+
+    //on disconnection
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+})
